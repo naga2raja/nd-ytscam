edda7dacd8a1f8cd012bf1ae0558035f1cad8c57
anbucabcs <anbua@cabcsgroup-india.com>
2021-06-01 11:03:56 +0530

    Route issue fix

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     //return redirect(RouteServiceProvider::HOME);
 });
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 });
 
